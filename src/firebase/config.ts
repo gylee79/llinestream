d957bd668c108022692cbd9f7c25d1cdbc4578f5
셀크리닉팀장 <cellclinicfamily@gmail.com>
2025-12-15 10:59:46 +0000

    도대체 왜 삭제가 안될까. 여기 파이어베이스 내용과는 모두 동일한거지?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:888420412846:web:75d82e40765479c8832c3f",
   "apiKey": "AIzaSyCuXs_szwwsAzEWC_0DAzjJloCLle68ZTI",
   "authDomain": "studio-6929130257-b96ff.firebaseapp.com",
-  "storageBucket": "studio-6929130257-b96ff.firebasestorage.app",
+  "storageBucket": "studio-6929130257-b96ff.appspot.com",
   "measurementId": "",
   "messagingSenderId": "888420412846"
 };
