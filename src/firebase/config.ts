4ff2d51885470e7fa250ed4e50b9f1b9c3879019
셀크리닉팀장 <cellclinicfamily@gmail.com>
2025-12-24 07:20:54 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-6929130257-b96ff",
-  "appId": "1:888420412846:web:75d82e40765479c8832c3f",
   "apiKey": "AIzaSyCuXs_szwwsAzEWC_0DAzjJloCLle68ZTI",
   "authDomain": "studio-6929130257-b96ff.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "888420412846"
+  "projectId": "studio-6929130257-b96ff",
+  "storageBucket": "studio-6929130257-b96ff.firebasestorage.app",
+  "messagingSenderId": "888420412846",
+  "appId": "1:888420412846:web:75d82e40765479c8832c3f"
 };
+
