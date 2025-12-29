5dd7c5003e32cd1258153b855b101369ae559e71
셀크리닉팀장 <cellclinicfamily@gmail.com>
2025-12-29 15:36:14 +0000

    이미지 슬라이더 구현 (courses/[courseId]/page.tsx):  강좌의 여러 소개 이미지를 보여주기 위해, 기존의

--- src/components/ui/dot-button.tsx
+++ src/components/ui/dot-button.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import React from 'react';
@@ -68,4 +67,3 @@ export const DotButton = React.forwardRef<HTMLButtonElement, DotButtonProps>((pr
 })
 
 DotButton.displayName = "DotButton"
-
