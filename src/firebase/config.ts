58640202f645dad1c8ae629e333a5422504ccdc4
셀크리닉팀장 <cellclinicfamily@gmail.com>
2026-01-04 15:47:01 +0000

    또 시작이네. storageBucket:주소 studio-6929130257-b96ff.firebasestorage.app야. 너

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyCuXs_szwwsAzEWC_0DAzjJloCLle68ZTI",
   "authDomain": "studio-6929130257-b96ff.firebaseapp.com",
   "projectId": "studio-6929130257-b96ff",
-  "storageBucket": "studio-6929130257-b96ff.firebasestorage.app",
+  "storageBucket": "studio-6929130257-b96ff.appspot.com",
   "messagingSenderId": "888420412846",
   "appId": "1:888420412846:web:75d82e40765479c8832c3f"
 };
