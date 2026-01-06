a9c42b871f2482d1a05c3c2d131ac57d0519d33f
셀크리닉팀장 <cellclinicfamily@gmail.com>
2026-01-06 03:10:18 +0000

    모바일버전에서 시청영상 3개정도 볼수있게 사이즈를 조정해줘 (_for element h2_)

--- src/components/shared/content-carousel.tsx
+++ src/components/shared/content-carousel.tsx
@@ -39,7 +39,7 @@ export default function ContentCarousel({ title, items, itemType }: ContentCarou
       >
         <CarouselContent>
           {items.map((item) => (
-            <CarouselItem key={item.id} className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6">
+            <CarouselItem key={item.id} className="basis-1/3 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6">
               <div className="p-1 h-full">
                 {itemType === 'course' ? (
                     <CourseCard course={item as Course} />
