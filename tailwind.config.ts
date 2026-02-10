ef4c46eeb59d206e55d1e5577c5a4275cf34948a
셀크리닉팀장 <cellclinicfamily@gmail.com>
2026-02-10 11:52:53 +0000

    랜딩페이지 홈화면이 정상적으로 뜨지 않아

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type {Config} from 'tailwindcss';
 
-export default {
+const config: Config = {
   darkMode: ['class'],
   content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -88,4 +88,6 @@ export default {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};
+
+export default config;
